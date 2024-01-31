102aece1790421f4389599573b2a7dbfcea83d9a
Pranoy Santhosh M <51205605+Pranoypauljr@users.noreply.github.com>
2024-01-31 16:59:26 +0530

    final commit

--- content-projection/src/app/app.component.ts
+++ content-projection/src/app/app.component.ts
@@ -1,28 +1,50 @@
-import { ChangeDetectorRef, Component, ViewChild } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AfterContentInit, ChangeDetectorRef, Component, OnInit, ViewChild } from '@angular/core';
 import { MultiselComponent } from './multisel/multisel.component';
+import { HelperService } from './helper.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor(private cdr: ChangeDetectorRef) { } 
+export class AppComponent implements OnInit, AfterContentInit{
+  constructor(private readonly helperservice:HelperService) { } 
   title = 'content-projection';
-  public selectedField:string="";
+  public selectedField:string="Name";
+  public field_set:string[]=[];
+  public curr_event:string='Name';
+  public size:number=5;
+  public index: number = 0;
   @ViewChild(MultiselComponent) multiselComponent!: MultiselComponent;
-
-  prevElem() {
-    this.multiselComponent.prevElem();
+  public prevElem($event: Event): void {
+    $event.preventDefault();
+    if (this.index > 0) {
+      this.index--;
+      this.showField(this.index);
+    }
   }
 
-  nextElem() {
-    this.multiselComponent.nextElem();
+  ngOnInit(): void {
+    console.log('app component ngOnInit')
   }
-  ngAfterViewInit() {
-    this.selectedField = this.multiselComponent.curr_event;
-    this.cdr.detectChanges();
+
+  public nextElem($event: Event): void {
+    
+    $event.preventDefault();
+    if (this.index <= this.size - 1) {
+      this.index+=1;
+      this.showField(this.index);
+    }
+  }
+  private showField(index:number): void {
+    if (this.multiselComponent.formFields) {
+      this.curr_event=this.field_set[index];
+      this.selectedField = this.curr_event;
+      return;
+    }
+  }
+  ngAfterContentInit(): void {
+    this.field_set=this.helperservice.field_list;
   }
 
 }
--- content-projection/src/app/multisel/multisel.component.ts
+++ content-projection/src/app/multisel/multisel.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChildren, QueryList, AfterContentInit, ChangeDetectorRef } from '@angular/core';
+import { Component, ContentChildren, QueryList, AfterContentInit, ChangeDetectorRef, OnInit } from '@angular/core';
 import { HelperService } from '../helper.service';
 
 @Component({
@@ -7,42 +7,6 @@ import { HelperService } from '../helper.service';
   styleUrls: ['./multisel.component.scss'],
   providers:[HelperService]
 })
-export class MultiselComponent implements AfterContentInit {
-  constructor(private readonly helperservice:HelperService, private cd: ChangeDetectorRef){}
-  @ContentChildren('formFields') formFields!: QueryList<any>;
-  public index: number = 0;
-  public field_set:string[]=[];
-  public curr_event:string='Name';
-  ngAfterContentInit(): void {
-    this.field_set=this.helperservice.field_list;
-    //this.showField(this.index);
-    this.cd.detectChanges();
-  }
-
-  public nextElem(): void {
-    if (this.index < this.formFields.length - 1) {
-      
-      this.index++;
-      this.showField(this.index);
-      this.cd.detectChanges();
-    }
-  }
-
-  public prevElem(): void {
-    if (this.index > 0) {
-      this.index--;
-      this.showField(this.index);
-      this.cd.detectChanges();
-    }
-  }
-
-  private showField(index:number): void {
-    if (this.formFields) {
-      // this.formFields.toArray().forEach((field, i) => {
-      //   field.style.display = i === this.index ? 'block' : 'none';
-      // });
-      this.curr_event=this.field_set[index];
-      return;
-    }
-  }
+export class MultiselComponent  {
+  @ContentChildren('formFields') formFields: QueryList<any>=new QueryList<any>();
 }
